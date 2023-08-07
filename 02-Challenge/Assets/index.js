48418efea9a29c561fa1eb1284fd753352e11160
Ruben <rderksen04@outlook.com>
2023-08-07 11:45:24 -0700

    CSS edits

--- 02-Challenge/Assets/index.js
+++ 02-Challenge/Assets/index.js
@@ -35,6 +35,34 @@ const questions=[
 ];
 //answer C is the correct answer, use this to comapre for cycling through
 
-function endScreen(){
-    //sets div properties to end displays
+// Selects element by class
+var timeEl = document.querySelector(".time");
+
+// Selects element by id
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 10;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      endScreen();
+    }
+
+  }, 1000);
 }
+
+// Function to create and append colorsplosion image
+function endScreen() {
+//make end screen
+
+}
+
+setTime();
