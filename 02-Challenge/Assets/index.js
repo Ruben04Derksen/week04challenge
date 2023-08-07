b9dea0af4bd218f741a8fb7e8fa75b5b4da1ca14
Ruben <rderksen04@outlook.com>
2023-08-06 23:19:55 -0700

    first day

---
+++ 02-Challenge/Assets/index.js
@@ -0,0 +1,40 @@
+//creat an object array of questions
+const questions=[
+{
+    question:"Which color is NOT in the rainbow?",
+    answer1 :"Orange",
+    answer2 :"Indigo",
+    answer3 :"White",
+    answer4 :"Red",
+    answerC :"White",
+},
+{ 
+   question : "What is the Netherlands national color?",
+    answer1 :"Blue",
+    answer2 :"Orange",
+    answer3 :"White",
+    answer4 :"Green",
+    answerC :"Orange",
+},
+{
+    question : "What is Scotands national animal?",
+    answer1 : "Unicorn",
+    answer2 : "Golfer",
+    answer3 : "Shepard",
+    answer4 : "Rabbit",
+    answerC : "unicorn",
+},
+{
+    question:"Which team has won the most Chmapions League titles?",
+    answer1:"Barcelona",
+    answer2:"Ajax",
+    answer3:"Real Madrid",
+    answer4:"Liverpool",
+    answerC:"Real Madrid",
+}
+];
+//answer C is the correct answer, use this to comapre for cycling through
+
+function endScreen(){
+    //sets div properties to end displays
+}
